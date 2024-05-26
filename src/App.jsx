17f8e9968e2e24e2db4c5ce5d7d6be0fe6149eb4
aydog4nn <zephyrflaner@gmail.com>
2024-05-26 12:07:26 +0300

    movie-search-page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Container from '@mui/material/Container';
+
+
+function App() {
+
+  return (
+    <Container style={{height:"fit-content"}}>
+        <div>
+            <h1>Movie Search Page</h1>
+            <div>
+                Film sayfası
+            </div>
+        </div>
+  </Container>
+  )
+}
+
+export default App
